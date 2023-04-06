37d9768cc66826aa5d0f3a0a5ff61de1328376f3
Clayton <persinger.clayton@gmail.com>
2023-04-05 18:22:00 -0700

    Combine multiple arrays in to a single array

---
+++ concat.js
@@ -0,0 +1,11 @@
+const concat = function(arr1, arr2){
+  for(const item of arr2){
+    arr1.push(item);
+  }
+  return arr1;
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
