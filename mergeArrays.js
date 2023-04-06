fd886dcfcc1e9d0656f7055ce0c8e61bfa55c9e2
Clayton <persinger.clayton@gmail.com>
2023-04-06 09:50:45 -0700

    Function to merge and sort arrays from lowest to highest numbers

---
+++ mergeArrays.js
@@ -0,0 +1,14 @@
+const merge = function(arr1, arr2){
+  
+  for(const item of arr2){
+    arr1.push(item);
+  }
+
+  arr1.sort()
+
+  return arr1;
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
